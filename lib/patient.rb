6d6654d53afe5d4c8ada9df756f66e1b1905ae36
Daniel Sun <danielsun12345@gmail.com>
2020-02-25 02:39:31 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Patient 
   attr_accessor :name
   
@@ -21,6 +22,10 @@ class Patient
   end 
   
   def doctors
+<<<<<<< HEAD
+=======
+    binding.pry 
+>>>>>>> 861bdfdc9754fdbc5a75673c6bbe33197d6c6470
     self.appointments.collect {|appointment| appointment.doctor}
   end 
   
